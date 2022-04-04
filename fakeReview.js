333bd42dd25d6baa0c6d8ff20586685f69c1d12a
akashpk41 <akashpk84100@gmail.com>
2022-04-04 08:07:12 +0600

    fake review data created

---
+++ fakeReview.js
@@ -0,0 +1,66 @@
+const data = [
+  {
+    name: "Luke Rota",
+    id:1,
+    rating: "4.5",
+    image:
+      "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+    review: `For someone who had never seen a Mad Max film before, I enjoyed every minute of this film
+
+     This is honestly one of the best action films of all time!
+     Arguably the best action film in years
+     I had no idea what to expect from this film to be honest but I was just blown away really!
+      I was honestly on the edge of my seat from start to finish!
+     The film may be short on plot but its absolutely crazy on action
+     The action is insane!
+     No shaky cam, no quick cut editing! You can see everything and you really do appreciate it too.
+     Its funny how the plot of the film is just one big car chase?
+
+     Tom Hardy is great as Max`,
+  },
+  {
+    name: "Ratty Hall",
+    id:2,
+    rating: "5",
+    image:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400/570a1745898621.58408191aee7a.jpg",
+    review: `A fabulous triumph in the history of cinema, and almost flawless for an action movie!  It is set in a dystopian place of desert, and bleak survival, for it is is engrained in their senses due to the violent tyrant Immortan Joe.  With this stunning, excellent example of dystopian action cinema, this movie proves to be overall shocking thrill ride to the senses, keeping you awake and at the edge of your own seat, showing to be one of the best action thrillers with fantastic performances from Tom Hardy as Max  `
+  },
+  {
+    name: "Abheet Gupta",
+    id:3,
+    rating: "4",
+    image:
+      "https://res.cloudinary.com/twenty20/image/private/s--l_fGasUf--/c_fit,w_640/g_south_east,l_overlay-twenty20-logo-stacked/v1/photosp/ig-536533319670356700_30966845.jpg",
+    review: ` Some movies are made purely for entertainment purposes and are full of jaw-dropping and cinematic moments. Mad Max: Fury Road (2015) is one of those movies which is low on substance but overcompensates in its story telling technique. Director George Miller brings us a high-octane thrill ride where just like its main character Max, audience too is never at rest and is left wanting for more. The director tries to bring forward a harsh world set in a dystopian future which is somewhat a mirror to our very own world or what it could become if everyone is reduced to a single instinct which is survival, just as Tom Hardys Max said in the opening of the film`
+  },
+  {
+    name: "John Angell",
+    id:4,
+    rating: "5",
+    image: "https://pbs.twimg.com/media/D8XfdElXUAATutj.jpg",
+    review: ` Borderline perfection. I grew up loving The Road Warrior (and wanting to kill myself after the 2nd half of Beyond Thunderdome... stupid kids...), and was more than a little apprehensive about Fury Road until I learned the studio had lost and the final cut would be Rated R.
+
+  Just a thrill-ride. The first 45 minutes are absolutely exhilarating, perfectly sprinkling world-building and exposition without sacrificing a drop of adrenaline `,
+  },
+  {
+    name: "Chris Gadbury",
+    id:5,
+    rating: "4.8",
+    image:
+      "https://i.pinimg.com/originals/32/38/6c/32386c72c7f2a8b5c1a10fc51c149cb1.jpg",
+    review: ` Mad Max: Fury Road - 2015
+
+  Get set and let's go on a perilous ride! George Miller set an ultimate trend of full action and violence followed by a soft story this time. So, lets vroom...
+
+  From the very beginning, the movie kicks in and sets the viewers to keep their eyes open until the very end. Almost the whole movie is covered by the action that too not just a normal action. Out of the world, rusty modified killer vehicles chasing one another getting the full attention of viewer  `,
+  },
+  {
+    name: "Jason Waynes",
+    id:6,
+    rating: "5",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Fjakemiller%2Fstatus%2F422093466295103488&psig=AOvVaw0-4PfaF39VcmJ_RFLArVaW&ust=1649123786208000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDO7fKm-fYCFQAAAAAdAAAAABAn",
+    review: ` No shaky cam, amazing wide shots, heart-stopping car stunts, dazzling effects, high octane intensity. This is one of the BEST ACTION MOVIES I HAVE EVER SCENE! Go watch this right now, and every other action movie will pale in comparison. I (sadly) havent seen the OG Mad Max movies and I still think this movie is a 10/10. A 10/10 for the camerawork, effects, DEFINITELY the stunts, performances, production design, and so much more. It feels realistic and raw while also being epically over the top and so good I even laughed out loud out is pure shock. `
+  }
+];
