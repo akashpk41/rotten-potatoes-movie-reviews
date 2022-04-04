e3dd51af499323a009b1670756a36ae042d3654e
akashpk41 <akashpk84100@gmail.com>
2022-04-04 19:31:25 +0600

    barchart updated

--- src/components/Dashboard/Chart/BarChart.jsx
+++ src/components/Dashboard/Chart/BarChart.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
 import useChart from "../../../hooks/useChart";
 
 const SpecialChart = () => {
   const [chart, setChart] = useChart();
   console.log(chart);
 
-  return <BarChart width={500} height={500} data={chart}>
-       <Bar dataKey='month' fill='#8884d8'/>
-       <XAxis dataKey='investment' />
-       <Tooltip />
-       <YAxis dataKey='revenue' type="monotone"
-        stroke="#8884d8" />
-  </BarChart>;
+  return (
+    <BarChart
+          width={500}
+          height={500}
+          data={chart}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="month" stackId="a" fill="#8884d8" />
+          <Bar dataKey="investment" stackId="a" fill="#82ca9d" />
+          <Bar dataKey="revenue" fill="#ffc658" />
+        </BarChart>
+  );
 };
 
 export default SpecialChart;
