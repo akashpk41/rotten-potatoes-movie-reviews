331376791d7e2972fea9ebb92972a0062a321123
akashpk41 <akashpk84100@gmail.com>
2022-04-04 08:39:39 +0600

    not found page added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-yellow-100 border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
+    <nav className="bg-yellow-100 text-center border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/" className="flex items-center">
           <span className="self-center text-2xl text-violet-900 whitespace-nowrap font-mono font-bold uppercase  ">
@@ -60,8 +60,7 @@ const Navbar = () => {
             <li>
               <Link
                 to="/home"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Home
               </Link>
--- src/components/Not_Found/NotFound.jsx
+++ src/components/Not_Found/NotFound.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import errorImg from "../../images/page-not-found.png";
 const NotFound = () => {
-     return (
-          <div>
-                <h1 className=' text-2xl text-center bg-red-500 p-3 ' > Sorry no page found  </h1>
-          </div>
-     );
+  return (
+    <div className="flex mx-3 md:w-4/5 md:mx-auto md:p-5  shadow-2xl rounded-sm my-10 flex-col-reverse md:flex-row items-center justify-center ">
+      <div className="left-side p-2    ">
+        <h1 className=" text-2xl text-red-600 ">
+          {" "}
+          Sorry !  The page you request was not found{" "}
+        </h1>
+      </div>
+      <div className="right-side  ">
+        <img
+          className=" rounded-sm my-3 w-96 "
+          src={errorImg}
+          alt="Page Not Found"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
